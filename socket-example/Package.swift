2e86c49b5cb1f4c68d627df8eb4ff85bf3e563ad
Tony Freeman <tonyfreeman@users.noreply.github.com>
2017-01-07 09:25:25 +0000

    Upgrade socket-example to Async example

--- socket-example/Package.swift
+++ socket-example/Package.swift
@@ -3,6 +3,8 @@ import PackageDescription
 let package = Package(
     name: "SocketExample",
     dependencies: [
-        .Package(url: "https://github.com/tris-foundation/socket.git", majorVersion: 0)
+        .Package(url: "https://github.com/tris-foundation/socket.git", majorVersion: 0),
+        .Package(url: "https://github.com/tris-foundation/async-dispatch.git", majorVersion: 0),
+        .Package(url: "https://github.com/tris-foundation/fiber.git", majorVersion: 0)
     ]
 )
--- socket-example/Sources/main.swift
+++ socket-example/Sources/main.swift
@@ -1,35 +1,45 @@
-import Foundation
-import Dispatch
+import Fiber
 import Socket
+import Dispatch
+import Foundation
+import AsyncFiber
+import AsyncDispatch
 
 let hey = [UInt8]("hey there!".utf8)
 
-DispatchQueue.global().async {
+let async = AsyncFiber()
+
+// see the initial commit to get the idea of AsyncDispatch
+// let async = AsyncDispatch()
+
+async.task {
     do {
-        let socket = try Socket()
+        let socket = try Socket(awaiter: async.awaiter)
         try socket.listen(at: "127.0.0.1", port: 7654)
         while true {
             let client = try socket.accept()
             let written = try client.write(bytes: hey)
         }
     } catch {
-        print("server socket error")
+        print("server socket error \(error)")
     }
 }
 
-DispatchQueue.global().async {
+async.task {
     do {
-        let socket = try Socket()
-        try socket.connect(to: "127.0.0.1", port: 7654)
+        for _ in 0..<10 {
+            let socket = try Socket(awaiter: async.awaiter)
+            try socket.connect(to: "127.0.0.1", port: 7654)
 
-        var buffer = [UInt8](repeating: 0, count: 100)
-        let read = try socket.read(to: &buffer)
+            var buffer = [UInt8](repeating: 0, count: 100)
+            let read = try socket.read(to: &buffer)
 
-        print(String(cString: buffer))
+            print(String(cString: buffer))
+        }
         exit(0)
     } catch {
-        print("client socket error")
+        print("client socket error \(error)")
     }
 }
 
-RunLoop.main.run()
+async.loop.run()
