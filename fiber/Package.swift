0cacea0be076aa4cc4899ea021c88df4509a4d51
Tony Freeman <tonyfreeman@users.noreply.github.com>
2017-04-23 21:15:54 +0000

    Fix Xcode project name conflict

--- fiber/Package.swift
+++ fiber/Package.swift
@@ -1,7 +1,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "fiber",
+    name: "example",
     dependencies: [
         .Package(url: "https://github.com/tris-foundation/fiber.git", majorVersion: 0)
     ]
