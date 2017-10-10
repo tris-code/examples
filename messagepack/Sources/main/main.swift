870ebfdc973939a39688a39403d764b4f91f0d6d
Tony Freeman <tonyfreeman@users.noreply.github.com>
2017-10-10 16:00:03 +0000

    [MessagePack] Remove do catch

--- messagepack/Sources/main/main.swift
+++ messagepack/Sources/main/main.swift
@@ -1,24 +1,21 @@
 import Stream
 import MessagePack
 
-do {
-    let hey = MessagePack("hey there!")
-    let bytes = try MessagePack.encode(hey)
-    if let original = String(try MessagePack.decode(bytes: bytes)) {
-        print("original value: \(original)")
-    }
-
-    var encoder = MessagePackWriter(OutputByteStream())
-    try encoder.encode("one")
-    try encoder.encode(2)
-    try encoder.encode(3.0)
-    let encoded = encoder.stream.bytes
-    
-    var decoder = MessagePackReader(InputByteStream(encoded))
-    let string = try decoder.decode(String.self)
-    let int = try decoder.decode(UInt8.self)
-    let double = try decoder.decode(Double.self)
-    print("decoded manually: \(string), \(int), \(double)")
-} catch {
-    print(error)
+let hey = MessagePack("hey there!")
+let bytes = try MessagePack.encode(hey)
+if let original = String(try MessagePack.decode(bytes: bytes)) {
+    print("original value: \(original)")
 }
+
+var encoder = MessagePackWriter(OutputByteStream())
+try encoder.encode("one")
+try encoder.encode(2)
+try encoder.encode(3.0)
+let encoded = encoder.stream.bytes
+
+var decoder = MessagePackReader(InputByteStream(encoded))
+let string = try decoder.decode(String.self)
+let int = try decoder.decode(UInt8.self)
+let double = try decoder.decode(Double.self)
+
+print("decoded manually: \(string), \(int), \(double)")
