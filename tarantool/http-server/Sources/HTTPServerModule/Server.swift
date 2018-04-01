aea8e8463822c5d8ab5e7eb1f1863adea843213d
Tony Freeman <tonyfreeman@users.noreply.github.com>
2018-04-01 22:40:04 +0000

    Update tarantool-http example

--- tarantool/http-server/Sources/HTTPServerModule/Server.swift
+++ tarantool/http-server/Sources/HTTPServerModule/Server.swift
@@ -1,13 +1,11 @@
-import TarantoolModule
-import MessagePack
-import HTTP
 import Log
+import HTTP
+import MessagePack
+import TarantoolModule
 
 struct SomeError: Swift.Error {}
 
-func runServer() throws {
-    let server = try Server(host: "0.0.0.0", port: 8080)
-
+func registerRoutes(in server: Server) throws {
     let schema = try Schema(Box())
 
     guard let space = schema.spaces["data"] else {
@@ -17,17 +15,10 @@ func runServer() throws {
 
     var counter = 0
 
-    struct Model {
-        let fields: [String]
-
-        init<T: Tarantool.Tuple>(_ tuple: T) {
-            self.fields = tuple.map { String($0 as! MessagePack)! }
-        }
-    }
-
     server.route(get: "/json") {
-        let tuples = try space.select(iterator: .all)
-        return try Response(body: tuples.map { Model($0).fields })
+        let rows = try space.select(iterator: .all).map { $0.unpack() }
+        let strings = rows.map { $0.compactMap{ $0.stringValue } }
+        return try Response(body: strings)
     }
 
     server.route(get: "/*") {
@@ -48,19 +39,21 @@ func runServer() throws {
 
         return try Response(body: result[1, as: String.self] ?? "not a string")
     }
-
-    try server.start()
 }
 
 @_silgen_name("entry_point")
 public func main() -> Int {
     async.use(Tarantool.self)
+
     async.task {
         do {
-            try runServer()
+            let server = try Server(host: "0.0.0.0", port: 8080)
+            try registerRoutes(in: server)
+            try server.start()
         } catch {
             Log.error(String(describing: error))
         }
     }
+
     return 0
 }
